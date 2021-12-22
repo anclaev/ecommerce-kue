5c5fe318bc58f85e566e5745f75ecf10a9de9f33
SAMARIN Artem <Artyom.Samarin@vaz.ru>
2021-12-22 09:50:20 +0400

    Подготовка проекта.

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App: React.FC = () => {
+  return (
+    <>
+      <h1>Тестирование по основам электронной коммерции</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/containers/Page/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Page: React.FC = () => {
+  return (
+    <main>
+      <h1>Тестирование по основам электронной коммерции</h1>
+    </main>
+  )
+}
+
+export default Page
